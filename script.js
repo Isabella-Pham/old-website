46f5506c8d1b6df690b1830302e2a25dd2e219be
Isabella Pham <isabella.aivy.pham@gmail.com>
2020-05-14 23:54:01 -0400

    created dropdown menu

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-body{
-  opacity:0;
-}
+var acc = document.getElementsByClassName("accordion");
+var i;
 
-$(window).load(function() {
-$("body").animate({ opacity: 1 }, 700);​
-});
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
